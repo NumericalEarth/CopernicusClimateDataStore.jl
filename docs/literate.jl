ea2871c802ba4f424b83f07b60f05567edcfb228
Gregory Wagner <wagner.greg@gmail.com>
2025-12-24 15:51:10 -0500

    add CDS keys or try to

--- docs/literate.jl
+++ docs/literate.jl
@@ -6,7 +6,11 @@ CairoMakie.activate!(type = "png")
 script_path = ARGS[1]
 literated_dir = ARGS[2]
 
+# Only execute examples if CDS credentials are available (set EXECUTE_EXAMPLES=true to force)
+# By default, skip execution since examples require ERA5 downloads
+execute_examples = get(ENV, "EXECUTE_EXAMPLES", "false") == "true"
+
 @time basename(script_path) Literate.markdown(script_path, literated_dir;
                                               flavor = Literate.DocumenterFlavor(),
-                                              execute = true)
+                                              execute = execute_examples)
 
