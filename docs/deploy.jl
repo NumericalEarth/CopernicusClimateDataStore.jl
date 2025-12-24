feb311dab2ac88a81eef3643296f1ba462e2a6f0
Gregory Wagner <wagner.greg@gmail.com>
2025-12-24 18:07:39 -0500

    update

--- docs/deploy.jl
+++ docs/deploy.jl
@@ -1,19 +1,20 @@
 using Documenter
 
-@info "Cleaning up temporary files..."
+@info "Cleaning up temporary .jld2 and .nc output created by doctests or literated examples..."
 
 for (root, _, filenames) in walkdir(@__DIR__)
     for file in filenames
-        if any(ext -> endswith(file, ext), (".jld2", ".nc", ".grib"))
+        if any(occursin(file), (r"\.jld2$", r"\.nc$", r"\.grib$"))
             rm(joinpath(root, file); force=true)
         end
     end
 end
 
-deploydocs(
+deploydocs(;
     repo = "github.com/NumericalEarth/CopernicusClimateDataStore.jl",
     devbranch = "main",
-    push_preview = true,
+    # Only push previews if all the relevant environment variables are non-empty. This is an
+    # attempt to work around https://github.com/JuliaDocs/Documenter.jl/issues/2048.
+    push_preview = all(!isempty, (get(ENV, "GITHUB_TOKEN", ""), get(ENV, "DOCUMENTER_KEY", ""))),
     forcepush = true,
 )
-
